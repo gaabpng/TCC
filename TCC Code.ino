c11d54ead77b3828c181393c78842d33dd3829d4
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2023-05-31 16:49:45 -0300

    Update TCC Code.ino
    começamos a implementar o código

--- TCC Code.ino
+++ TCC Code.ino
@@ -1,15 +1,15 @@
-//Bibliotecas
-#include <WiFi.h>
-#include <PubSubClient.h>
-#include <ESPmDNS.h>
-#include <DHT.h>
-#include <SSD1306.h>
- 
-//Net Setup
-#define NET_SSID "Wokwi-GUEST"
-#define NET_PASSWORD ""
+// Bibliotecas
+#include "WiFi.h"
+#include "PubSubClient.h"
+#include "ESPmDNS.h"
+#include "DHTesp.h"
+#include "heltec.h"
 
-//MQTT Setup
+// Net Setup
+#define NET_SSID "LABProv"
+#define NET_PASSWORD "labprov1"
+
+// MQTT Setup
 #define MQTT_ID "tcctermostato-esp32-0800313131"
 #define MQTT_BROKER "broker.hivemq.com"
 #define MQTT_PORT 1883
@@ -17,67 +17,69 @@
 #define MQTT_DHT_TEMP_TOPIC "tcc_termostato_temp"
 #define MQTT_DHT_HUMD_TOPIC "tcc_termostato_humd"
 
-//Cliente de rede
+// Cliente de rede
 WiFiClient espClient;
 
-//Cliente MQTT
+// Cliente MQTT
 PubSubClient MQTT(espClient);
 
-//Variáveis que armazenam as informações que são enviadas ao broker
+// Variáveis que armazenam as informações que são enviadas ao broker
 char millis_str[10] = "";
 char temp_str[10] = "";
 char humd_str[10] = "";
 
-//Display
-SSD1306 display(0x3c, 21, 22);
+// Definição da Tela e Sensor DHT22
+#define BAND 915E6 // Escolha a frequência
+DHTesp dht;
 
-//Sensor DHT22
-DHT dht(15, DHT22);
+// Variavéis do Sensor DHT22
+float currentTemp;
+float currentHumidity;
 
-//Temperatura
-float t;
+// Protótipo da função
+void getTemp();
+void getHumidity();
+void sendPacket();
 
-//Umidade
-float h;
+// Função de conexão Wi-Fi
+void setupWifi()
+{
 
-
-void setupWifi() {
-
-//Configura a conexão à rede sem fio
-  if (WiFi.status() == WL_CONNECTED)
+    if (WiFi.status() == WL_CONNECTED)
         return;
-  
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(NET_SSID);
-  
-  WiFi.begin(NET_SSID, NET_PASSWORD);
-  
-  while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-  }
-  
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(NET_SSID);
+
+    WiFi.begin(NET_SSID, NET_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
+// Função de conexão ao broker MQTT
+void setupMQTT()
+{
 
-void setupMQTT() {
-  
-//Informa qual broker e porta deve ser conectado
     MQTT.setServer(MQTT_BROKER, MQTT_PORT);
-   
-   while (!MQTT.connected()) 
+
+    while (!MQTT.connected())
     {
         Serial.print("*Tentando se conectar ao Broker MQTT: ");
         Serial.println(MQTT_BROKER);
-        if (MQTT.connect(MQTT_ID)) 
+        if (MQTT.connect(MQTT_ID))
         {
             Serial.println("Conectado com sucesso ao broker MQTT!");
-        } 
+        }
         else
         {
             Serial.println("Falha ao reconectar no broker.");
@@ -87,57 +89,138 @@ void setupMQTT() {
     }
 }
 
-void setup(void) {
+// Função que ler a temperatura e atibiu a variável currentTemp.
+void getTemp()
+{
 
-//Configura o baudrate da comunicação serial
-  Serial.begin(115200);
+    // Atribuindo a variavel float a função
+    float temperature = dht.getTemperature();
 
-  dht.begin();
-  display.init();
-  display.clear();
- 
-  setupWifi();
+    // Verifica se houve mudança na temperatura
+    if (temperature != currentTemp)
+    {
+        currentTemp = temperature;
 
-  setupMQTT();
+        // Liga o LED
+        digitalWrite(LED, HIGH);
 
+        // Espera 500 milissegundos
+        delay(500);
+
+        // Desliiga o LED
+        digitalWrite(LED, LOW);
+
+        // Espera 500 milissegundos
+        delay(500);
+    }
+    delay(1000);
 }
- 
-void loop(void) {
 
-//Strings de temperatura e umidade
-  h = dht.readHumidity();
-  t = dht.readTemperature();
+// Função que ler a Umidade e atibiu a variável currentHumidity.
+void getHumidity()
+{
+    // Atribuindo a variavel float a função
+    float humidity = dht.getHumidity();
 
-//Configuração do Display
-  display.clear();
-  display.drawRect(10, 10, 80, 50);
-  display.setFont(ArialMT_Plain_16);
-  display.drawString(20, 14, (String)t+" °C");
-  display.drawString(20, 35, (String)h+" %");
-  display.flipScreenVertically();
-  display.display();
-  
-//Monitor Serial
-  Serial.print("Time: ");
-  Serial.println(millis_str);
-  Serial.println("Temp: " + (String)t+ "°C");
-  Serial.println("Humidity: " + (String)h+ "%");
-  Serial.println("---");
+    // Verifica se houve mudança na temperatura
+    if (humidity != currentHumidity)
+    {
+        currentHumidity = humidity;
 
-//Envia Segundos
-  sprintf(millis_str, "%d", millis());
-  MQTT.publish(MQTT_MILLIS_TOPIC, millis_str);
+        // Liga o LED
+        digitalWrite(LED, HIGH);
 
-//Envia Temperatura
-  sprintf(temp_str, "%s", (String)t);
-  MQTT.publish(MQTT_DHT_TEMP_TOPIC, temp_str);
-  
- //Envia Umidade
-  sprintf(humd_str, "%s", (String)h);
-  MQTT.publish(MQTT_DHT_HUMD_TOPIC, humd_str);
+        // Espera 500 milissegundos
+        delay(500);
 
-//Configuração de Setup e Delay
-  setupWifi();
-  setupMQTT();
-  delay(1000);
+        // Desliiga o LED
+        digitalWrite(LED, LOW);
+
+        // Espera 500 milissegundos
+        delay(500);
+    }
+    delay(1000);
+}
+
+// Função principal (setup)
+void setup()
+{
+
+    // inicializa o LED
+    pinMode(LED, OUTPUT);
+
+    // Habilita diversas Configurações do Lora32
+    Heltec.begin(true, true, true, true, BAND);
+    /*(
+      true = Habilita o Display,
+      true = Heltec.Heltec.Heltec.LoRa Disable,
+      true = Habilita debug Serial,
+      true = Habilita o PABOOST,
+      BAND = Frequência BAND.
+    )*/
+
+    // Primeira Tela: Exibe os criadores do projeto enquanto de conecta ao Wi-Fi e ao Broker MQTT
+    Heltec.display->init();
+    Heltec.display->setContrast(255);
+    Heltec.display->clear();
+    Heltec.display->setFont(ArialMT_Plain_24);
+    Heltec.display->drawString(0, 0, "IFresources");
+    Heltec.display->setFont(ArialMT_Plain_16);
+    Heltec.display->drawString(0, 25, "@ifrjniteroi");
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->drawString(0, 45, "Conectando ao Wi-Fi...");
+    Heltec.display->display();
+    delay(1000);
+
+    // inicializa o DHT no pino 25
+    dht.setup(25, DHTesp::DHT22);
+
+    // Atribuindo as variaveis as funções
+    currentTemp = dht.getTemperature();
+    currentHumidity = dht.getHumidity();
+
+    // Configuração de Conexões
+    setupWifi();
+    setupMQTT();
+}
+
+// Função de repetição (loop)
+void loop()
+{
+    // Funções de Leitura de Temperatura e Umidade
+    getTemp();
+    getHumidity();
+
+    // Segunda Tela: Exibe os valores de temperatura na tela do Lora32.
+    Heltec.display->clear();
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+    Heltec.display->setFont(ArialMT_Plain_16);
+    Heltec.display->drawString(30, 5, "Temperatura"); //"Enviando"
+    Heltec.display->drawString(33, 30, (String)currentTemp);
+    Heltec.display->drawString(78, 30, "°C");
+    Heltec.display->display();
+
+    // Monitor Serial
+    Serial.print("Time: ");
+    Serial.println(millis_str);
+    Serial.println("Temp: " + (String)currentTemp + "°C");
+    Serial.println("Humidity: " + (String)currentHumidity + "%");
+    Serial.println("---");
+
+    // Envia Segundos
+    sprintf(millis_str, "%d", millis());
+    MQTT.publish(MQTT_MILLIS_TOPIC, millis_str);
+
+    // Envia Temperatura
+    sprintf(temp_str, "%s", (String)currentTemp);
+    MQTT.publish(MQTT_DHT_TEMP_TOPIC, temp_str);
+
+    // Envia Umidade
+    sprintf(humd_str, "%s", (String)currentHumidity);
+    MQTT.publish(MQTT_DHT_HUMD_TOPIC, humd_str);
+
+    // Configuração de Conexões e Configuração de Delay
+    setupWifi();
+    setupMQTT();
+    delay(1000);
 }
